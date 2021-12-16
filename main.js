67613c326ec31a80d9ef0005d8b0cf46f267a4fc
gennadiySinigur <gennadiysinigur@gmail.com>
2021-12-16 12:49:47 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+const burger = document.getElementById('burger');
+const sideMenu = document.getElementById('side-menu');
+const close = document.getElementById('close');
+
+burger.addEventListener('click', () => {
+  sideMenu.style.display = 'block';
+
+  document.body.style.overflow = "hidden";
+
+});
+
+close.addEventListener('click', () => {
+  sideMenu.style.display = 'none';
+
+  document.body.style.overflow = "";
+
+});
+
